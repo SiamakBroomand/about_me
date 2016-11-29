d690899bed325035bc9e555024de79b6fd1fb2a8
Siamak Broomand <broomand.siamk85@gmail.com>
2016-11-29 14:44:59 -0800

    finished question1 and made the text blue

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+'use strict';
+
 alert('I am alive!!!');
+//Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical.
+// Or not. It's up to you! It's your project!
+//For this stage of the buiiding of this project, DO NOT USE FUNCTIONS! We'll refactor later.
+//The questions in the guessing game must require a mix of yes/no answers, and user input must accept //either
+// y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input //and accommodate users entering all-caps Y/N or YES/NO answers, too.
+//Useful and descriptive console.log() messages in the JS are well written and correctly displaying to the
+//browser console for each question of the guessing game.
+var question1 = prompt ('Does Sam have any cats?').toLowerCase();
+
+//if (condition is true) {
+  //do sth;
+  //mayebe do something more;
+  //etc. untile done;
+
+// } else {
+  //do othe thing;
+  //and mayebe yet another thing;
+//}
+
+if (question1 === 'yes' || question1 === 'y') {
+  alert('Yes, Sam has three cats!');
+} else if (question1 === 'no' || question1 === 'n'){
+  alert('sorry, you are wrong! He has three cats!');
+} else {
+  alert(' Sorry you have no idea about him!!');
+}
