7f8589007ac7549d2b05c4474556f8e5d6bb898d
Siamak Broomand <broomand.siamk85@gmail.com>
2016-11-29 16:11:24 -0800

    removed comment from javasscript

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 'use strict';
 
 alert('This is all about you!!!');
-//Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical.
-// Or not. It's up to you! It's your project!
-//For this stage of the buiiding of this project, DO NOT USE FUNCTIONS! We'll refactor later.
-//The questions in the guessing game must require a mix of yes/no answers, and user input must accept //either
-// y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input //and accommodate users entering all-caps Y/N or YES/NO answers, too.
-//Useful and descriptive console.log() messages in the JS are well written and correctly displaying to the
-//browser console for each question of the guessing game.
+
 
 var userresponse1 = prompt ('Have you been to school?').toLowerCase();
 
