bab2ed81e20fdb4dbf2a2f557b793b60e70eb113
Siamak Broomand <broomand.siamk85@gmail.com>
2016-11-30 21:42:45 -0800

    added tally but just works in consolelog also added consol log to js

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 'use strict';
 
+//declare a variable called `rightAnswers` or the like at the top of your JS and assign it
+// a value of 0.
+
+//Every time a user gets a correct answer, in the same spot where you alert them that they got
+//a right answer, add one to that variable.
+
+//After the last question, that variable will hold the number of questions they got right.
+//if (user got a right answer) {
+  //tell them so;
+  //add one to their tally of correct answers;
+//} else {
+  //tell them they got the wrong answer;
+//}
 alert('This is all about you!!!');
 alert('we are going to answer aseries of yes/no questions.')
 
+var rightAnswers = 0;
 
-var userresponse1 = prompt ('Have you been to school?').toLowerCase();
 
-if (userresponse1 === 'yes' || userresponse1 === 'y') {
+var userResponse1 = prompt ('Have you been to school?').toLowerCase();
+
+if (userResponse1 === 'yes' || userResponse1 === 'y') {
   alert('Seems you are literate :)');
-} else if (userresponse1 === 'no' || userresponse1 === 'n'){
+  rightAnswers +=1;
+  console.log('user said ' + userResponse1 + ' to this question' );
+} else if (userResponse1 === 'no' || userResponse1 === 'n'){
   alert('Are you telling the truth?!');
 } else {
   alert('Try to answer yes or no next time!')
@@ -18,6 +35,7 @@ var userResponse2 = prompt ('Did you like school?').toLowerCase();
 
 if (userResponse2 === 'yes' || userResponse2 === 'y'){
   alert('You must have had a lot of fun in there');
+  rightAnswers +=1;
 } else if (userResponse2 === 'no' || userResponse2 === 'n'){
   alert('That must have been tough to go to school everyday!');
 } else {
@@ -27,6 +45,7 @@ if (userResponse2 === 'yes' || userResponse2 === 'y'){
 var userResponse3 = prompt ('Do you have enough experience in coding?').toLowerCase();
 if (userResponse3 === 'yes' || userResponse3 === 'y') {
   alert('I hope you are not a beginner!');
+  rightAnswers += 1;
 } else if (userResponse3 === 'no' || userResponse3 === 'n') {
   alert('No worries, not everybody knows coding!');
 } else {
@@ -36,6 +55,7 @@ if (userResponse3 === 'yes' || userResponse3 === 'y') {
 var userResponse4 = prompt ('Are you looking for a valuable job?').toLowerCase();
 if(userResponse4 === 'yes' || userResponse4 === 'y'){
   alert('Bravo, You are among the ones who want a change.');
+  rightAnswers +=1;
 } else if (userResponse4 === 'no' || userResponse4 ==='n') {
   alert('Do what you are doing and be on the HOPE PLAN');
   console.log(userResponse4 + ' never works. ');
@@ -46,31 +66,13 @@ if(userResponse4 === 'yes' || userResponse4 === 'y'){
 var userResponse5 = prompt ('Do you consider yourself hardworking and ambitious?').toLowerCase();
 if (userResponse5 === 'yes' || userResponse5 ==='y'){
   alert('You are the right person to make your life.');
+  rightAnswers+=1;
 } else if (userResponse5 === 'no' || userResponse5 === 'n'){
   alert('Read some success stories!!');
 } else {
   alert('Be more active!!!');
 }
 
-
-
-
-//var response6 = 0;
-  //var counter = 0;
-  //while (response6 !== 26 && counter < 4){
-    //response6 = parseInt(prompt('Q: In how many hours does a chicken lay one egg? \n Hint: It\'s less than 30!\nYou have 4 tries!'));
-    //console.log('userName thinks the answer is: ' + response6);
-    //counter += 1;
-    //if (response6 > 26){
-      //alert('Your number it greater than the answer!');
-    //} //else if (response6 === 26) {
-      //alert('You\'re right, chickens take ' + response6 + ' hours to lay one egg!');
-    //} else if (isNaN(response6)) {
-      //alert('Hey! Don\'t be a smartass!');
-    //} else {
-      //alert('Your number is lesser than the answer!');
-    //}
-  //}
 var userResponse6 = 0;
 var counter = 0;
 while (userResponse6 !== 32 && counter <4){
@@ -81,6 +83,7 @@ while (userResponse6 !== 32 && counter <4){
     alert ('Your guess is greater than my age!');
   } else if (userResponse6 === 32){
     alert('You are right! I am ' + userResponse6 + 'years old.');
+    rightAnswers+=1;
   } else {
     alert (' Your guess is lower than my age!');
   }
@@ -100,9 +103,16 @@ while (counter1 < 6) {
   }
   if (isMatch === true){
     alert ('You are damn right');
+    rightAnswers+=1;
     break;
   } else {
     alert('Try again, you are wrong!');
   }
   counter1 += 1;
+}
+
+if (userResponse1=== rightAnswers && userResponse2 === rightAnswers && userResponse3 === rightAnswers && userResponse4 === rightAnswers && userResponse5 === rightAnswers && userResponse6 === rightAnswers && userResponse7 === rightAnswers ){
+  alert('You have ' + rightAnswers + 'answers');
+} else if (userResponse1 !== rightAnswers && userResponse2!== rightAnswers && userResponse3!== rightAnswers && userResponse4 !== rightAnswers && userResponse5 !== rightAnswers && userResponse6!== rightAnswers && userResponse7 !== rightAnswers){
+  alert('you got some wrong answers')
 }
