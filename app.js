4f17811c4f2dd1d6829680134aa86878bd173011
Siamak Broomand <broomand.siamk85@gmail.com>
2016-11-30 17:25:53 -0800

    Hopefully git is fixed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 alert('This is all about you!!!');
+alert('we are going to answer aseries of yes/no questions.')
 
 
 var userresponse1 = prompt ('Have you been to school?').toLowerCase();
@@ -49,4 +50,59 @@ if (userResponse5 === 'yes' || userResponse5 ==='y'){
   alert('Read some success stories!!');
 } else {
   alert('Be more active!!!');
+}
+
+
+
+
+//var response6 = 0;
+  //var counter = 0;
+  //while (response6 !== 26 && counter < 4){
+    //response6 = parseInt(prompt('Q: In how many hours does a chicken lay one egg? \n Hint: It\'s less than 30!\nYou have 4 tries!'));
+    //console.log('userName thinks the answer is: ' + response6);
+    //counter += 1;
+    //if (response6 > 26){
+      //alert('Your number it greater than the answer!');
+    //} //else if (response6 === 26) {
+      //alert('You\'re right, chickens take ' + response6 + ' hours to lay one egg!');
+    //} else if (isNaN(response6)) {
+      //alert('Hey! Don\'t be a smartass!');
+    //} else {
+      //alert('Your number is lesser than the answer!');
+    //}
+  //}
+var userResponse6 = 0;
+var counter = 0;
+while (userResponse6 !== 32 && counter <4){
+  userResponse6 = parseInt (prompt ('Q: How old am I? \n Hint: It is between 30 and forty!\n You have 4 tries!'));
+  console.log('You think I am ' + userResponse6 + 'years old.');
+  counter += 1;
+  if (userResponse6 > 32){
+    alert ('Your guess is greater than my age!');
+  } else if (userResponse6 === 32){
+    alert('You are right! I am ' + userResponse6 + 'years old.');
+  } else {
+    alert (' Your guess is lower than my age!');
+  }
+}
+
+var language = ['english', 'farsi', 'french', 'italian'];
+var counter1 = 0;
+var isMatch = false;
+while (counter1 < 6) {
+  var userResponse7 = prompt(' What languages can I speak?').toLowerCase();
+  for (var i = 0; i < language.length; i++){
+    console.log('you think I can speak ' + userResponse7 + '!');
+    if (userResponse7 === language[i]){
+      isMatch = true;
+      break;
+    }
+  }
+  if (isMatch === true){
+    alert ('You are damn right');
+    break;
+  } else {
+    alert('Try again, you are wrong!');
+  }
+  counter1 += 1;
 }
